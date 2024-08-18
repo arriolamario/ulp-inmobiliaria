a3ec5899c7406555c37d994d521aab798461003a
Paulo Cabrera <paulocabrera90@gmail.com>
2024-08-18 03:44:05 -0300

    Agrego el get para obtener todos los inquilinos y script para datos

---
+++ ProyectoInmobiliaria/Controllers/InquilinoController.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoInmobiliaria.Models;
+using ProyectoInmobiliaria.Repositorio;
+
+namespace ProyectoInmobiliaria.Controllers;
+
+public class InquilinoController: Controller {
+
+    private readonly ILogger<HomeController> _logger;
+    private IConfiguration _Configuration;
+    private RepositorioInquilino _repositorioInquilino;
+
+    public InquilinoController(ILogger<HomeController> logger, IConfiguration configuration) {
+        _logger = logger;
+        _Configuration = configuration;
+        _repositorioInquilino = new RepositorioInquilino(_Configuration);
+    }
+
+    public IActionResult Index() {        
+        return View(_repositorioInquilino.GetInquilinos());
+    }
+}
---
+++ ProyectoInmobiliaria/Repositorio/RepositorioInquilino.cs
@@ -0,0 +1,41 @@
+namespace ProyectoInmobiliaria.Repositorio;
+
+using ProyectoInmobiliaria.Models;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+public class RepositorioInquilino: RepositorioBase {
+    public RepositorioInquilino(IConfiguration configuration): base(configuration) {
+    }
+
+    public List<Inquilino> GetInquilinos() {
+        List<Inquilino> resultInquilinos = new List<Inquilino>();
+
+        string query = @$"select {nameof(Inquilino.Id)}, 
+                            {nameof(Inquilino.Dni)}, 
+                            {nameof(Inquilino.Nombre)}, 
+                            {nameof(Inquilino.Apellido)}, 
+                            {nameof(Inquilino.Telefono)}, 
+                            {nameof(Inquilino.Email)}, 
+                            {nameof(Inquilino.Direccion)}, 
+                            {nameof(Inquilino.Fecha_Creacion)}, 
+                            {nameof(Inquilino.Fecha_Actualizacion)} 
+                    from inquilino;";
+
+        resultInquilinos = this.ExecuteReaderList<Inquilino>(query, (reader) => {
+            return new Inquilino() {
+                Apellido = reader["apellido"].ToString() ?? "",
+                Dni = reader["dni"].ToString() ?? "",
+                Email = reader["email"].ToString() ?? "",
+                Nombre = reader["nombre"].ToString() ?? "",
+                TelefonoArea = reader["telefono"].ToString().Split('-')[0] ?? "",
+                TelefonoNumero = reader["telefono"].ToString().Split('-')[1] ?? "",
+                Direccion = reader["direccion"].ToString() ?? "",
+                Id = int.Parse(reader["id"].ToString() ?? "0"),
+                Fecha_Creacion = DateTime.Parse(reader["fecha_creacion"].ToString() ?? "0"),
+                Fecha_Actualizacion = DateTime.Parse(reader["fecha_actualizacion"].ToString() ?? "0")
+            };
+        });
+        return resultInquilinos;
+    }
+}
