9b01baaa2deb5ededd57348cec5fee28586e5fdb
Mario Arriola <arriola.mario.90@gmail.com>
2024-09-20 21:56:22 -0300

    fixes

--- DataBase/script.sql
+++ DataBase/script.sql
@@ -1,7 +1,3 @@
--- Crear la base de datos si no existe
-CREATE DATABASE IF NOT EXISTS inmo_ca;
-USE inmo_ca;
-
 -- Crear la tabla 'propietario'
 CREATE TABLE IF NOT EXISTS propietario (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -54,7 +50,7 @@ CREATE TABLE IF NOT EXISTS inmueble (
     coordenada_lat VARCHAR(255) NOT NULL,
     coordenada_lon VARCHAR(255) NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
-    activo TINYINT(1) NOT NULL DEFAULT 1,
+    activo TINYINT(1) NOT NULL DEFAULT 0,
     id_propietario INT,
 	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -72,16 +68,10 @@ CREATE TABLE IF NOT EXISTS usuario (
     avatar_url TEXT,
     rol ENUM('empleado', 'administrador') NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-<<<<<<< HEAD
-    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    telefono varchar(100) DEFAULT NULL
-);
-=======
     fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
->>>>>>> 15301ec9a526a2a4d6ff5dc7fdd12f949ba1b746
 
 CREATE TABLE IF NOT EXISTS contrato (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -113,8 +103,8 @@ CREATE TABLE IF NOT EXISTS pago (
     fecha_pago datetime NOT NULL,
     detalle varchar(255) NOT NULL DEFAULT '',
     importe decimal(10, 2) NOT NULL,
-    multa decimal(10, 2) DEFAULT NULL,
-    estado enum('Pagado','Anulado','Pendiente') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Pendiente',
+    multa decimal(10, 2) NOT NULL,
+    estado varchar(50) NOT NULL DEFAULT '',
     creado_por_id int NOT NULL,
     anulado_por_id int DEFAULT NULL,
     fecha_anulacion datetime DEFAULT NULL,
@@ -123,24 +113,6 @@ CREATE TABLE IF NOT EXISTS pago (
     FOREIGN KEY (creado_por_id) REFERENCES usuario (Id) ON DELETE NO ACTION ON UPDATE CASCADE,
     FOREIGN KEY (anulado_por_id) REFERENCES usuario (Id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
-
--- Insertar datos en la tabla 'propietario'
-INSERT INTO propietario (dni, nombre, apellido, telefono, email, direccion)
-VALUES 
-('20345678', 'Juan', 'Pérez', '11-5555-1234', 'juan.perez@example.com', 'Calle Falsa 123, Buenos Aires'),
-('27456321', 'María', 'López', '11-5555-5678', 'maria.lopez@example.com', 'Avenida Siempreviva 742, Buenos Aires'),
-('30112233', 'Carlos', 'García', '11-5555-9101', 'carlos.garcia@example.com', 'Plaza Mayor 5, Buenos Aires'),
-('32987654', 'Lucía', 'Martínez', '11-5555-1122', 'lucia.martinez@example.com', 'Calle Real 10, Buenos Aires'),
-('34123456', 'Luis', 'Fernández', '11-5555-3344', 'luis.fernandez@example.com', 'Avenida del Sol 21, Buenos Aires');
-
--- Insertar datos en la tabla 'inquilino'
-INSERT INTO inquilino (dni, nombre, apellido, telefono, email, direccion, activo)
-VALUES
-('10000001', 'Laura', 'Sánchez', '555-2345', 'laura.sanchez@example.com', 'Calle Primavera 12, Ciudad Jardín', 1),
-('10000002', 'Ricardo', 'Hernández', '555-6789', 'ricardo.hernandez@example.com', 'Avenida Libertad 45, Sector 3', 1),
-('10000003', 'Beatriz', 'Moreno', '555-9876', 'beatriz.moreno@example.com', 'Calle de la Luna 88, Barrio Nuevo', 1),
-('10000004', 'Fernando', 'Álvarez', '555-3456', 'fernando.alvarez@example.com', 'Plaza del Sol 34, Centro Histórico', 1),
-('10000005', 'Carmen', 'Fernández', '555-4567', 'carmen.fernandez@example.com', 'Avenida del Mar 78, Playa Norte', 1);
 
 insert into tipo_inmueble (descripcion) 
 values ('Duplex'),
@@ -157,3 +129,4 @@ INSERT INTO usuario (email, password_hash, nombre, apellido, avatar_url, rol)
 VALUES
 ('admin@yopmail.com', 'myl4T6FgkMUdldPQ96rZUnNYn0ho5fyVIc39WWFLd8Y=', 'NommbreAdm', 'ApellidoAdm', '', 'administrador'),
 ('empleado@yopmail.com', 'myl4T6FgkMUdldPQ96rZUnNYn0ho5fyVIc39WWFLd8Y=', 'NombreEmp', 'ApellidoEmp', '', 'empleado');
+
